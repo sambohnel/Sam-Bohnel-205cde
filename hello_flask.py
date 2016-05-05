81b0ce56c722715b48ec786b4ebf168b852990a2
sambohnel <sambohnel@yahoo.co.uk>
2016-05-05 18:21:48 +0000

    added routing files

---
+++ hello_flask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__);
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.getenv('PORT', 8080))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
---
+++ routing.py
@@ -0,0 +1,31 @@
+from flask import Flask, make_response, abort, redirect, render_template 
+
+app = Flask(__name__);
+
+@app.route('/')
+def index():
+    return render_template("HomePage.html")
+    
+@app.route('/90spage')
+def ninetyspage():
+    return render_template("90spage.html")
+    
+@app.route('/00s')
+def twos():
+    return render_template("00s.html")
+    
+@app.route('/10s')
+def tens():
+    return render_template("10s.html")
+    
+@app.route('/CL')
+def CL():
+    return render_template("CL.html")
+
+@app.route('/WC')
+def WC():
+    return render_template("WC.html")
+    
+@app.route('/EURO')
+def EURO():
+    return render_template("EURO.html")
